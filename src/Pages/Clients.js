be72fe2968f3d2fef8483b5757c6965d9e751778
Maulik Patel <km8295@gmail.com>
2021-05-25 10:42:58 +0530

    add client logos

--- src/Pages/Clients.js
+++ src/Pages/Clients.js
@@ -7,22 +7,22 @@ const Clients = () => {
         <div className="row">
           <div className="col-md-3 col-6">
             <div className="single-client mt-30 text-center">
-              <img src="assets/images/client_logo_01.png" alt="Logo" />
+              <img src="https://picsum.photos/100/100" alt="Logo" />
             </div>
           </div>
           <div className="col-md-3 col-6">
             <div className="single-client mt-30 text-center">
-              <img src="assets/images/client_logo_02.png" alt="Logo" />
+              <img src="https://picsum.photos/100/100" alt="Logo" />
             </div>
           </div>
           <div className="col-md-3 col-6">
             <div className="single-client mt-30 text-center">
-              <img src="assets/images/client_logo_03.png" alt="Logo" />
+              <img src="https://picsum.photos/100/100" alt="Logo" />
             </div>
           </div>
           <div className="col-md-3 col-6">
             <div className="single-client mt-30 text-center">
-              <img src="assets/images/client_logo_04.png" alt="Logo" />
+              <img src="https://picsum.photos/100/100" alt="Logo" />
             </div>
           </div>
         </div>
